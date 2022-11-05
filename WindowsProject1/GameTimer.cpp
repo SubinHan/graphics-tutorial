23b3799854ebdceed3af29dd7b46711bbda129f7
5ubin <hanidiot@gmail.com>
2022-11-05 17:47:49 +0900

    GameTimer added

---
+++ WindowsProject1/GameTimer.cpp
@@ -0,0 +1,82 @@
+#include "GameTimer.h"
+
+GameTimer::GameTimer() : secondsPerCount(0.0), deltaTime(-1.0), baseTime(0),
+pausedTime(0), stopTime(0), prevTime(0), currentTime(0), isStopped(false)
+{
+	__int64 countsPerSec;
+	QueryPerformanceFrequency((LARGE_INTEGER*)&countsPerSec);
+	secondsPerCount = 1.0 / (double)countsPerSec;
+}
+
+float GameTimer::GameTime() const
+{
+	if (isStopped)
+	{
+		return (float)(((stopTime - pausedTime) - baseTime) * secondsPerCount);
+	}
+
+	return (float)(((currentTime - pausedTime) - baseTime) * secondsPerCount);
+}
+
+float GameTimer::DeltaTime() const
+{
+	return (float)deltaTime;
+}
+
+void GameTimer::Reset()
+{
+	__int64 currentTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+
+	baseTime = currentTime;
+	prevTime = currentTime;
+	stopTime = 0;
+	isStopped = false;
+}
+
+void GameTimer::Start()
+{
+	if (!isStopped)
+		return;
+
+	__int64 startTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&startTime);
+
+	pausedTime += startTime - stopTime;
+	prevTime = startTime;
+	stopTime = 0;
+	isStopped = false;
+}
+
+void GameTimer::Stop()
+{
+	if (isStopped)
+		return;
+
+	__int64 currentTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+
+	stopTime = currentTime;
+	isStopped = true;
+}
+
+void GameTimer::Tick()
+{
+	if (isStopped)
+	{
+		deltaTime = 0.0;
+		return;
+	}
+
+	__int64 currentTime;
+	QueryPerformanceCounter((LARGE_INTEGER*)&currentTime);
+	this-> currentTime = currentTime;
+
+	deltaTime = (currentTime - prevTime) * secondsPerCount;
+	prevTime = currentTime;
+
+	if (deltaTime < 0.0)
+	{
+		deltaTime = 0.0;
+	}
+}
---
+++ WindowsProject1/GameTimer.h
@@ -0,0 +1,28 @@
+#pragma once
+#include <Windows.h>
+
+class GameTimer
+{
+public:
+	GameTimer();
+
+	float GameTime() const;
+	float DeltaTime() const;
+
+	void Reset();
+	void Start();
+	void Stop();
+	void Tick();
+
+private:
+	double secondsPerCount;
+	double deltaTime;
+
+	__int64 baseTime;
+	__int64 pausedTime;
+	__int64 stopTime;
+	__int64 prevTime;
+	__int64 currentTime;
+
+	bool isStopped;
+};
