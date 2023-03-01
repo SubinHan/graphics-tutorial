64bece4fd9d571be1f0e4de0d23fa601cd561e5a
SubinHan <hanidiot@gmail.com>
2023-03-01 12:57:36 +0900

    The bug fixed that the meshes looked wireframe but not solid.

--- WindowsProject1/07/ShapeApp.cpp
+++ WindowsProject1/07/ShapeApp.cpp
@@ -553,7 +553,7 @@ void ShapeApp::BuildPSOs()
         shaders["opaquePS"]->GetBufferSize()
     };
     opaquePsoDesc.RasterizerState = CD3DX12_RASTERIZER_DESC(D3D12_DEFAULT);
-    opaquePsoDesc.RasterizerState.FillMode = D3D12_FILL_MODE_WIREFRAME;
+    opaquePsoDesc.RasterizerState.FillMode = D3D12_FILL_MODE_SOLID;
     opaquePsoDesc.BlendState = CD3DX12_BLEND_DESC(D3D12_DEFAULT);
     opaquePsoDesc.DepthStencilState = CD3DX12_DEPTH_STENCIL_DESC(D3D12_DEFAULT);
     opaquePsoDesc.SampleMask = UINT_MAX;
